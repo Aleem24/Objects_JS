7ecef224ad9426bdb7d4e0aed94a8fdfc6c5b86d
Aleem24 <md.samiulaleem16@gmail.com>
2024-11-22 18:26:46 +0600

    one commit

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 let student = {
-    name : "Aleem",
+    name : "Md.Abdullaheel Aleem",
     age : 10,
     grade : 4,
 };
 
 
- let obj1 = `Hello I am ${student.name} and I read in grade ${student["grade"]}.`;
+ let obj1 = ` I am ${student.name} and I read in grade ${student["grade"]}.`;
 
 document.getElementById("r1").innerHTML = obj1;
+
+
+
+let marks = Math.abs(10);
+document.write("First-term C.T  marks : " + marks +"<br>")
+
+let value = Math.acos(-1);
+document.write("Mid-term C.T  marks : " + value + "<br>");
+
+let variable = Math.asin(1);
+document.write("Final-term C.T  marks : " + variable + "<br>");
+
+
+
+
+
+
+
+
